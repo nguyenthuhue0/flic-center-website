c0ccd202da58571d97a2f321cea4f25d44db9e93
Tuanngo-02 <thanhtuanhht0211@gmail.com>
2025-07-15 09:24:24 +0700

    init commit

---
+++ src/Layout.jsx
@@ -0,0 +1,38 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import Home from "./pages/user/Home";
+
+const NotFound = () =>{
+    return(
+        <div className="container mt-3 alert alert-danger">404.Not found data</div>
+    );
+}
+const Layout = () => {
+    return (
+        <>
+        <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="/" element={<App />} >
+                <Route index element={<Home />} />
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+        <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+        />
+        </>
+    )
+}
+export default Layout;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Layout/>
+    </BrowserRouter>
+  </StrictMode>,
+)
