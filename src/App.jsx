77884d5d5cc5afc5d87074548d75a30b19b66f7f
nguyenthuhue0 <nguyenthuhue.vku@gmail.com>
2025-07-22 14:57:57 +0700

    first commmit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
 
 export default App
+
+
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,24 +4,29 @@ import Register from "./pages/auth/Register";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/user/Home";
+import Introduce from "./pages/user/Introduce";
+import RegisterForm from "./pages/user/RegisterForm";
 
-const NotFound = () =>{
-    return(
+const NotFound = () => {
+    return (
         <div className="container mt-3 alert alert-danger">404.Not found data</div>
     );
 }
 const Layout = () => {
     return (
         <>
-        <Routes>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="/" element={<App />} >
-                <Route index element={<Home />} />
-            </Route>
-            <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-        <ToastContainer
+            <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="/" element={<App />} >
+                    <Route index element={<Home />} />
+                    <Route path="introduce" element={<Introduce />} />
+                    <Route path="registerform" element={<RegisterForm />} />
+                    <Route path="home" element={<Home />} />
+                </Route>
+                <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+            <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -31,7 +36,7 @@ const Layout = () => {
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
-        />
+            />
         </>
     )
 }
