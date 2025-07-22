6f79b05ee065713156ed7c5ece701d125b867f2a
thuylanh04 <vothithuylanh2004@gmail.com>
2025-07-22 14:27:58 +0700

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,11 @@ import Register from "./pages/auth/Register";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/user/Home";
+import Schedule from "./pages/user/Schedule";
+import News from "./pages/user/News";
+import NewsDetail from "./pages/user/NewsDetail";
+import MaterialsDashboard from "./pages/user/MaterialsDashboard";
+
 
 const NotFound = () =>{
     return(
@@ -18,6 +23,14 @@ const Layout = () => {
             <Route path="register" element={<Register />} />
             <Route path="/" element={<App />} >
                 <Route index element={<Home />} />
+                <Route path="schedule" element={<Schedule />} />
+                <Route path="news" element={<News/>} />
+                <Route path="/news/:id" element={<NewsDetail />} />
+                
+            </Route>
+            <Route path="/lecturer" element={<App />} >
+                <Route path="materialsdashboard" element={<MaterialsDashboard/>} />
+                
             </Route>
             <Route path="*" element={<NotFound />}></Route>
         </Routes>
